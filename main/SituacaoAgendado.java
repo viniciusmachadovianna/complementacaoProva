61e99fceb12a0a12dff41fb3ff76036549ac880c
viniciusmachadovianna <viniciusmachadovianna@outlook.com>
2026-06-15 23:21:12 -0300

    SituacaoAgendado

---
+++ main/SituacaoAgendado.java
@@ -0,0 +1,27 @@
+package main;
+
+import main.Atendimento;
+
+public class SituacaoAgendado implements SituacaoAtendimento {
+
+    @Override
+    public void iniciar(Atendimento a) {
+        a.setSituacao(new SituacaoEmAtendimento());
+        a.notificarObservadores();
+    }
+
+    @Override
+    public void finalizar(Atendimento a) {
+        throw new IllegalStateException(
+            "Não é possível finalizar um atendimento que ainda não foi iniciado.");
+    }
+
+    @Override
+    public void cancelar(Atendimento a) {
+        a.setSituacao(new SituacaoCancelado());
+        a.notificarObservadores();
+    }
+
+    @Override
+    public String getNome() { return "Agendado"; }
+}
